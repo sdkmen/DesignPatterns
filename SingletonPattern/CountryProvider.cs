d72a717df2f4bf87552b9f78a58a072bbaea27bd
Seyfullah Dikmen <sdkmenn@gmail.com>
2024-09-20 14:33:48 +0300

    Add singleton pattern

---
+++ SingletonPattern/CountryProvider.cs
@@ -0,0 +1,32 @@
+﻿using SingletonPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class CountryProvider
+    {
+        private static CountryProvider instance = null;
+        public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+        private new List<Country> Countries { get; set; }
+
+        private CountryProvider()
+        {
+            // retrieving data from db
+            Task.Delay(2000).GetAwaiter().GetResult();
+
+            Countries = new List<Country>()
+            {
+                new Country(){Name = "Turkey"},
+                new Country(){Name = "Belgium"},
+            };
+        }
+
+        public int CountryCount => Countries.Count;
+
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using SingletonPattern;
+
+//var countryProvider = new CountryProvider();
+
+//await CountryProvider.Instance.GetCountries();
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country.Name);
+}
+
+// another service
+
+//var countryProvider1 = new CountryProvider(); not allowed bc it has private constructor
+
+var countries1 = await CountryProvider.Instance.GetCountries();
+
+Console.WriteLine(CountryProvider.Instance.CountryCount);
+Console.WriteLine(DateTime.Now.ToLongTimeString());
